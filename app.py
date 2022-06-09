b9bb148aa89c4bfd2341490ded3e6f0e80a8b64c
samar <samar.ziouziou@esen.tn>
2022-06-09 16:40:13 +0200

    v1

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ from sklearn import model_selection
 from sklearn.svm import SVC
 #Scaling our columns
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 import numpy as np
 import pickle as p
-import pandas as pa
+import pandas as pd
 import json
 from sklearn.preprocessing import StandardScaler
 
@@ -24,13 +25,26 @@ cors = CORS(app, resource={
 })
 @app.route('/api/', methods=['POST'])
 def makecalc():
-    data = request.get_json()
-    json_ = request.json
-    response = np.array2string(model.predict(data))
-    response.headers.add('Access-Control-Allow-Headers', 'x-csrf-token')
-
-    return jsonify(response)
-
+    #data = request.get_json()
+    #json_ = request.json
+    #response = np.array2string(model.predict(data))
+    #return jsonify(response)
+    iris = pd.read_csv("IRIS.csv")
+    # splitting the dataset
+    x = iris.drop("species", axis=1)
+    y = iris["species"]
+    x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                        test_size=0.2,
+                                                        random_state=0)
+    # training the model
+    from sklearn.neighbors import KNeighborsClassifier
+    model.fit()
+    # giving inputs to the machine learning model
+    # features = [[sepal_length, sepal_width, petal_length, petal_width]]
+    features = np.array([[5, 2.9, 1, 0.2]])
+    # using inputs to predict the output
+    prediction = knn.predict(features)
+    print("Prediction: {}".format(prediction))
 
 if __name__ == '__main__':
     modelfile = 'voting.pkl'
